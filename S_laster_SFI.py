86152b08376ccb26566c3a02014049f1639801f5
mirai-work <mfkensub@gmail.com>
2026-05-15 16:13:17 +0900

    Update S_laster_SFI.py

--- S_laster_SFI.py
+++ S_laster_SFI.py
@@ -749,8 +749,8 @@ class App:
                 pyxel.text(70, 120, "INVINCIBLE(U/D):", 7); pyxel.text(140, 120, f"[{inv_status}]", 11 if self.invincible else 13)
                 pyxel.text(65, 128, "DEBUG MODE ACTIVE", 3)
             if pyxel.frame_count % 30 < 15:
-                pyxel.text(83, 95, "PRESS SPACE OR START ", 6)
-             pyxel.text(72, 140, "(C)MIRAI WORK / M.T 2026", 7)
+               pyxel.text(83, 95, "PRESS SPACE OR START ", 6)
+               pyxel.text(72, 140, "(C)MIRAI WORK / M.T 2026", 7)
 
         elif self.state == "GAMEOVER":
             pyxel.text(int(W/2-28), int(H/2-5), "MISSION FAILED", 11)
